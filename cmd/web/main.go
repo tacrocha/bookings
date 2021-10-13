a7b112162bcfb32dfe2918e5431f141ffdc5ca85
Tacio Rocha <tacio.rocha@gmail.com>
2021-10-12 23:52:31 -0400

    Initial commit

---
+++ cmd/web/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/alexedwards/scs/v2"
+	"github.com/tacrocha/bookings/pkg/config"
+	"github.com/tacrocha/bookings/pkg/handlers"
+	"github.com/tacrocha/bookings/pkg/render"
+)
+
+const portNumber = ":8080"
+
+var app config.AppConfig
+var session *scs.SessionManager
+
+// main is the main application function
+func main() {
+	// change this to true when in production
+	app.InProduction = false
+
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction
+
+	app.Session = session
+
+	tc, err := render.CreateTemplateCache()
+	if err != nil {
+		log.Fatal("cannot create template cache", err)
+	}
+
+	app.TemplateCache = tc
+	app.UseCache = false
+
+	repo := handlers.NewRepo(&app)
+	handlers.NewHandlers(repo)
+	render.NewTemplates(&app)
+
+	fmt.Printf("Starting application on port %s\n", portNumber)
+
+	srv := &http.Server{
+		Addr:    portNumber,
+		Handler: routes(&app),
+	}
+
+	err = srv.ListenAndServe()
+	log.Fatal(err)
+}
